c13c1fdd99c9e8d46b65f21b2f5a1850c1a9286a
Michael Tighe <michaelt@zumasys.com>
2019-12-06 17:00:48 -0800

    Update project to conform to standard C# conventions, replace string concatenation w/ StringBuilder, UrlBuilder & built-in string interpolation.

--- entities/Question.cs
+++ entities/Question.cs
@@ -4,25 +4,43 @@ using System.Collections.Generic;
 namespace HelpjuiceConverter.Entities
 {
     class Question
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public int? views { get; set; }
-        public int account_id { get; set; }
-        public int accessibility { get; set; }
-        public string description { get; set; }
-        public string email { get; set; }
-        public Boolean is_published { get; set; }
-        public string codename { get; set; }
-        public int language_id { get; set; }
-        public int? position { get; set; }
-        public int? source_id { get; set; }
-        public int? source_type { get; set; }
-        public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
-        public string answer_sample { get; set; }
-        public string long_answer_sample { get; set; }
-        public List<Category> categories { get; set; }
-        public List<string> tags { get; set; }
+    {        
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public int? Views { get; set; }
+        
+        public int AccountId { get; set; }
+        
+        public int Accessibility { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string Email { get; set; }
+
+        public bool IsPublished { get; set; }
+        
+        public string CodeName { get; set; }
+        
+        public int LanguageId { get; set; }
+        
+        public int? Position { get; set; }
+
+        public int? SourceId { get; set; }
+
+        public int? SourceType { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string AnswerSample { get; set; }
+
+        public string LongAnswerSample { get; set; }
+        
+        public List<Category> Categories { get; set; }
+        
+        public List<string> Tags { get; set; }
     }
 }
